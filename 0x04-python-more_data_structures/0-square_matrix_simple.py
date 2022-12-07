bce562c1417f4e7a6922264df95560ef96a36173
Orio-Ola <samueloriola212@gmail.com>
2022-12-07 14:32:25 +0000

    All task

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,2 @@
+def square_matrix_simple(matrix=[]):
+    return [list(map(lambda x: x **2, row) for row in matrix]
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,2 @@
+solution to:
+0x04. Python - More Data Structures: Set, Dictionary
