e7fde10596c1de035e0e432d3e1dab8afb49ed7e
Orio-Ola <samueloriola212@gmail.com>
2022-12-20 15:08:23 +0000

    using private attributes in python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
+"""Defines a square """
+
 
 class Square:
-    """
-    class square that has attributes:
-        size
-    """
+    """Represents a square"""
+
     def __init__(self, size):
-        """
-        the initialization function for the square class
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
         """
         self.__size = size
