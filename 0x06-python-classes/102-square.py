22804adf0e5b5953b10180f994ba17a577383051
Orio-Ola <samueloriola212@gmail.com>
2022-12-20 16:01:59 +0000

    using custom comparison methods for classes

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -3,86 +3,51 @@
 
 
 class Square:
-    """
-    class square that has attributes:
-        size
-    some attributes are protected from input.
-    """
+    """defines a square"""
 
     def __init__(self, size=0):
+        """Create a Square
+        Args: size: length of a side of Square
         """
-        initialization function for our square clasee
-        """
-        if self.__validate_size(size):
-            self.__size = size
-
-
-    def __eq__(self, other):
-        """
-        used by == to check equality
-        """
-        return (self.area() == other.area())
-
-
-    def __ne__(self, other):
-        """
-        used by != to check equality
-        """
-        return (self.area() != other.area())
-
-    def __lt__(self, other):
-        """
-        used by < to check equality
-        """
-        return (self.area() < other.area())
-
-    def __le__(self, other):
-        """
-        used by <= to check equality
-        """
-        return (self.area() <= other.area())
-
-    def __gt__(self, other):
-        """
-        used by > to check equality
-        """
-        return (self.area() > other.area())
-
-    def __ge__(self, other):
-        """
-        used by >= to check equality
-        """
-        return (self.area() >= other.area())
+        self.__size = size
 
     @property
     def size(self):
-        """
-        getter for size attribute
+        """"The propery of size as the len of a side of Square
+        Raises:
+            TypeError: if size != int
+            ValueErrorr: if size < 0
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        setter for size attribute
-        """
-        if self.__validate_size(value):
-            self.__size = value
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     def area(self):
+        """Get the area of a Square
+        Returns: The size squared
         """
-        calculates the area of the square
-        """
-        return self.__size ** 2
+        return self.__size * self.__size
 
-    def __validate_size(self, size):
-        """
-        validates the size, checking for errors
-        """
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            return True
-        return False
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
