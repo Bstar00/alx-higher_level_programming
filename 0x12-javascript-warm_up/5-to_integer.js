0bcdbc65e3682a7896b157e92796f969250e48b2
Bstar00 <samueloriola212@gmail.com>
2023-03-13 09:56:19 +0000

    An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,4 +3,4 @@
  * if the first argument can be converted to an integer
  * */
 const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? "Not a number" : `My number: ${num}`);
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
