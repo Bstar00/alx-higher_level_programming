13b092f9f55503a62060ada440041ac2d6400240
Bstar00 <samueloriola212@gmail.com>
2023-03-31 17:52:09 -0700

    postemail

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,8 +11,8 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")# data must be in bytes
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    req_uest = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req_uest) as response:
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))
