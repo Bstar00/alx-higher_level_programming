7f1ecf37719d8e951faba8e056251e5b6595960c
Bstar00 <samueloriola212@gmail.com>
2023-04-28 05:15:29 +0000

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# It takes in a URL, sends a request to the URL,and displays the size of the body of the response
+# Takes in a URL, sends a request to the URL,and displays the size of the body of the response
 curl -sX GET $1 -L
