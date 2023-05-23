5b04d79fabcf4d3a15ac242580bed1efd6b9e9d4
Bstar00 <samueloriola212@gmail.com>
2023-05-23 12:56:35 +0000

    request

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// A script that gets the contents of a webpage and stores it in a file.
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (_err, _res, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
